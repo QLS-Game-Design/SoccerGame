ca941c675d8cfe4fe6dc910cd06e6b3fe05b5e83
Jolteous <wirelesskhurram@outlook.com>
2022-11-16 18:44:35 -0800

    The base game has been added.
    I made the two players, controls and jump working. I added a ball and a short script for the ball.

---
+++ Assets/Scenes/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    private bool canJump;
+    public float speed = 5.0f;
+    public float jump_height = 4.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision other) {
+        canJump = true;
+    }
+    private void OnCollisionExit(Collision other) {
+        canJump = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if ( canJump && Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(transform.TransformDirection(Vector3.up) * jump_height);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        { 
+            transform.Translate(-1 * Vector3.forward * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.W)) { 
+            transform.Translate(Vector2.left * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * speed);
+        }
+       
+        
+    }
+    
+}
---
+++ Assets/Scenes/SoccerBall.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoccerBall : MonoBehaviour
+{
+    public float speed = 5f;
+ 
+ void OnTriggerEnter (Collider col)
+     {
+     if(col.tag == "Player")
+         {
+         GetComponent<Rigidbody>().AddForce(transform.TransformDirection(Vector3.forward) * speed );
+         }
+     }
+}
+
