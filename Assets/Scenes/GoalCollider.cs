fde8de4cbe988f41ac8a5daba5ceb7fe44e10603
charlesjin123 <71144512+charlesjin123@users.noreply.github.com>
2023-01-17 21:13:13 -0800

    fixed movement and invis walls

--- Assets/Scenes/GoalCollider.cs
+++ Assets/Scenes/GoalCollider.cs
@@ -7,27 +7,36 @@ public class GoalCollider : MonoBehaviour
     public GameObject ball;
     public GameObject player1;
     public GameObject player2;
+
+    private Vector3 player1Start;
+    private Vector3 player2Start;
+    private Vector3 ballStart;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        player1Start = player1.transform.position;
+        player2Start = player2.transform.position;
+        ballStart = ball.transform.position;
     }
 
     // Update is called once per frame
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "BALL"){
             Debug.Log("SCORE");
-            ball.transform.position = Vector3.left;
+            ball.transform.position = ballStart;
+            // ball.transform.position = Vector3.left;
             ball.transform.rotation = Quaternion.identity;
             ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
             
             player1.transform.rotation = Quaternion.identity;
-            player1.transform.position = new Vector3(0.501464844f,0.430000305f,-9.26629353f);
+            player1.transform.position = player1Start;
+            // player1.transform.position = new Vector3(0.501464844f,0.430000305f,-9.26629353f);
             player1.GetComponent<Rigidbody>().velocity = Vector3.zero;
 
             player2.transform.rotation = Quaternion.identity;
-            player2.transform.position = new Vector3(0.501464844f,0.430000305f,8.35370636f);
+            player2.transform.position = player2Start;
+            // player2.transform.position = new Vector3(0.501464844f,0.430000305f,8.35370636f);
             player2.GetComponent<Rigidbody>().velocity = Vector3.zero;
 
         }
--- Assets/Scenes/PlayerMovement.cs
+++ Assets/Scenes/PlayerMovement.cs
@@ -8,11 +8,44 @@ public class PlayerMovement : MonoBehaviour
     private bool canJump;
     public float speed = 5.0f;
     public float jump_height = 4.0f;
-    
+    public int playerNum;
+
+    //private KeyCode upKey;
+    //private KeyCode downKey;
+    //private KeyCode leftKey;
+    //private KeyCode rightKey;
+    private KeyCode jumpKey;
+    private string inputAxisHorizontal;
+    private string inputAxisVertical;
+
+    private Rigidbody rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        if (playerNum == 1)
+        {
+            //upKey = KeyCode.W;
+            //downKey = KeyCode.S;
+            //leftKey = KeyCode.A;
+            //rightKey = KeyCode.D;
+            inputAxisHorizontal = "Player1Horizontal";
+            inputAxisVertical = "Player1Vertical";
+            jumpKey = KeyCode.Space;
+        }
+        else if (playerNum == 2)
+        {
+            //upKey = KeyCode.UpArrow;
+            //downKey = KeyCode.DownArrow;
+            //leftKey = KeyCode.LeftArrow;
+            //rightKey = KeyCode.RightArrow;
+            inputAxisHorizontal = "Player2Horizontal";
+            inputAxisVertical = "Player2Vertical";
+            jumpKey = KeyCode.RightShift;
+        }
     }
     private void OnCollisionEnter(Collision other) {
         if(other.gameObject.tag == "FLOOR"){
@@ -25,28 +58,52 @@ public class PlayerMovement : MonoBehaviour
         }
     }
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if ((canJump && Input.GetKey(KeyCode.Space)))
+        if ((canJump && Input.GetKey(jumpKey)))
         {
             GetComponent<Rigidbody>().AddForce(transform.TransformDirection(Vector3.up) * jump_height);
         }
-        if (Input.GetKey(KeyCode.D) && transform.position.x < 1)
-        {
-            transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        }
-        if (Input.GetKey(KeyCode.A))
-        { 
-            transform.Translate(-1 * Vector3.forward * Time.deltaTime * speed);
-        }
-        if (Input.GetKey(KeyCode.W)) { 
-            transform.Translate(Vector2.left * Time.deltaTime * speed);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector2.right * Time.deltaTime * speed);
-        }
-       
+
+        Vector3 v = new Vector3(Input.GetAxisRaw(inputAxisVertical), 0, Input.GetAxisRaw(inputAxisHorizontal)).normalized * Time.deltaTime * speed;
+        v.y = rb.velocity.y;
+        rb.velocity = v;
+
+        //if (Input.GetKey(rightKey) && transform.position.x < 1)
+        //{
+        //    Vector3 v = Vector3.forward * Time.deltaTime * speed;
+        //    v.y = rb.velocity.y;
+        //    rb.velocity = v;
+        //    // transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //}
+        //if (Input.GetKey(leftKey))
+        //{
+        //    Vector3 v = -1 * Vector3.forward * Time.deltaTime * speed;
+        //    v.y = rb.velocity.y;
+        //    rb.velocity = v;
+        //    // transform.Translate(-1 * Vector3.forward * Time.deltaTime * speed);
+        //}
+        //if (Input.GetKey(upKey)) 
+        //{
+        //    Vector3 v = Vector2.left * Time.deltaTime * speed;
+        //    v.y = rb.velocity.y;
+        //    rb.velocity = v;
+        //    // transform.Translate(Vector2.left * Time.deltaTime * speed);
+        //}
+        //if (Input.GetKey(downKey))
+        //{
+        //    Vector3 v = Vector2.right * Time.deltaTime * speed;
+        //    v.y = rb.velocity.y;
+        //    rb.velocity = v;
+        //    // transform.Translate(Vector2.right * Time.deltaTime * speed);
+        //}
+
+        //if (!Input.GetKey(upKey) && !Input.GetKey(downKey) && !Input.GetKey(leftKey) && !Input.GetKey(rightKey))
+        //{
+        //    Vector3 v = new Vector3(0, 0, 0);
+        //    v.y = rb.velocity.y;
+        //    rb.velocity = v;
+        //}
 
     }
     
